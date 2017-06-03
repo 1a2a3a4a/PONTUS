8938b9df04ddf987389d752adb2c0b7ba6241ebd
Tony <tonywang3334@gmail.com>
2017-06-03 19:30:46 +0200

    Added dummy text to Candidate

--- landing/src/EmployerDirectory/Candidate.js
+++ landing/src/EmployerDirectory/Candidate.js
@@ -7,8 +7,9 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import People from 'material-ui/svg-icons/social/people'
 
 class Candidate extends Component{
+
     render(){
-        const style = {margin: 5};
+    const style = {margin: 5};
         return(
             <div>
                 <MuiThemeProvider>
@@ -17,6 +18,28 @@ class Candidate extends Component{
                             style={style}
                     />
                 </MuiThemeProvider>
+                <CandidateInformation/>
+            </div>
+        )
+    }
+}
+
+
+class CandidateInformation extends Component{
+
+    render(){
+        const styles ={
+            display: 'inline-block',
+            margin: '0.6em',
+            position: 'absolute',
+            float: 'right',
+        };
+        return(
+
+            <div style={styles}>
+                <span>Lorum Ipsum etc.</span>
+                <br/>
+                <span>More latin words etc.</span>
             </div>
         )
     }

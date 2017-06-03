ac50c49309ffbd2cbcad89c6633e6285818f9734
Simon Ljus <simon.ljus94@gmail.com>
2017-06-03 17:20:22 +0200

    welcomepage not rendering from index

--- landing/src/WelcomePage.js
+++ landing/src/WelcomePage.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 //material-ui stuff
@@ -10,7 +9,9 @@ class WelcomePage extends Component {
     return (
       <div className="WelcomePage">
         <div>
+
           <RaisedButton label="Default" />
+
         </div>
       </div>
     );
--- landing/src/index.js
+++ landing/src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import WelcomePage from './WelcomePage'
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<WelcomePage />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();

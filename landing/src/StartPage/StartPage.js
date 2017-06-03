09c3045956930fc3cfdcef459df451cf0f2e01e2
Simon Ljus <simon.ljus94@gmail.com>
2017-06-03 16:47:47 +0200

    material

---
+++ landing/src/StartPage/StartPage.js
@@ -0,0 +1,25 @@
+/**
+ * Created by simonljus on 2017-06-03.
+ */
+import React, { Component } from 'react';
+
+
+class StartPage extends Component{
+    constructor(props){
+        super()
+        this.state = {
+            value: null,
+        };
+    }
+    render(){
+
+
+        return(
+            <div className="StartPageContainer">
+
+            </div>
+
+        )
+    }
+}
+export default StartPage
